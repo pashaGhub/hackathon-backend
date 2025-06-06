78231b3529ece1cbcb4dc9703411c7db730cf8b0
pavel <pawelsucharew@gmail.com>
2025-06-06 22:16:27 +0300

    test CI/CD

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hackathon API is running!!!');
+  res.send('Hackathon API is running!');
 });
 
 app.listen(port, () => {
